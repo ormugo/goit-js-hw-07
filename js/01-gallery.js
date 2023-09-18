d3155c2cbd7e72247aecd07d288f5222b231794f
ormugo <orladismunozgomez@gmail.com>
2023-09-18 18:29:08 -0500

    ACTIVIDAD_7_GOIT

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryMarkUp = document.querySelector(".gallery");
+
+const galleryEl = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+
+galleryMarkUp.insertAdjacentHTML("beforeend", galleryEl);
+
+galleryMarkUp.addEventListener("click", onImgClick);
+
+function onImgClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName != "IMG") {
+    return;
+  }
+
+  const modal = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+
+    {
+      onShow: () => window.addEventListener("keydown", onEscKeyPress),
+      onClose: () => window.removeEventListener("keydown", onEscKeyPress),
+    }
+  );
+
+  modal.show();
+
+  function onEscKeyPress(evt) {
+    if (evt.code === "Escape") {
+      modal.close();
+    }
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,20 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryMarkUp = document.querySelector(".gallery");
+
+const galleryEl = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+  )
+  .join("");
+
+galleryMarkUp.insertAdjacentHTML("beforeend", galleryEl);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
